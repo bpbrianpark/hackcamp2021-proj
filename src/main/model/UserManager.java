37bb4434c1ba328c4d271fb56bd454d5d68989da
Jason Liang <jasonliang2002@gmail.com>
2021-11-14 12:10:46 -0800

    Added comments to each method cause I was peer pressured into doing so, and cause how each method has a clear purpose written in plain english

--- src/main/model/UserManager.java
+++ src/main/model/UserManager.java
@@ -4,30 +4,39 @@ import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+// Jason
+// Represents the User manager class that deals with many users
 public class UserManager {
     private ArrayList<User> listOfUsers;
 
+    // Constructor
+    // EFFECTS: creates a new list of users
     public UserManager() {
         listOfUsers = new ArrayList<>();
     }
 
+    // GETTERS
     public List<User> getUsers() {
         return this.listOfUsers;
     }
 
+    // MODIFIES: This
+    // EFFECTS: adds the given user into the list of user
     public void addUser (User u) {
         if (!listOfUsers.contains(u)) {
             listOfUsers.add(u);
         }
     }
 
+    // MODIFIES: This
+    // EFFECTS: removes the given user form the list of users
     public void removeUser (User u) {
         if (listOfUsers.contains(u)) {
             listOfUsers.remove(u);
         }
     }
 
-    //sort by their ratios completed/asked the higher the ratio the higher the user is up on the list
+    // EFFECTS: Returns a sorted list of users given a list of users by their Completed/Asked favour ratio (Descending)
     public List<User> sortedUsers(ArrayList<User> arr) {
         int n = arr.size();
         User temp;
